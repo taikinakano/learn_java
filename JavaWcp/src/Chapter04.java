466c851775962adc6def52aa3e1e0c01213f38bb
taikinakano <kabayaki3323@gmail.com>
2021-08-02 02:22:34 +0900

    studying chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 extends Variable01 {
+
+	 public static void main(String[] args) {
+		 String str1;
+		 str1 = "中野太貴";
+		 System.out.println(str1);
+
+		 str1 = "唯我独尊";
+		 System.out.println(str1);
+
+		 int num1;
+		 num1 = 777;
+		 System.out.println(num1);
+	 }
+}

6100c19a7d6aff2603d4bdb7b84fd41dd51298e5
taikinakano <kabayaki3323@gmail.com>
2021-08-04 15:38:09 +0900

    learn Chapter15

---
+++ JavaWcp/src/While01.java
@@ -0,0 +1,10 @@
+
+public class While01 extends Chapter07 {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number < 50) {
+            number *= 2;
+            System.out.println("While01 = " + number);
+        }
+    }
+}
---
+++ JavaWcp/src/extend/phone/Flying.java
@@ -0,0 +1,12 @@
+package extend.phone;
+
+public interface Flying {
+
+    // メソッド（抽象メソッド）
+    void fly();
+
+    // defaultメソッド
+    default void powerOff() {
+        System.out.println("飛んでいる場合は落下します。");
+    }
+}

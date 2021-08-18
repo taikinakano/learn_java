158675c36af1535880bd0e72dc2e6f9649ce0a81
taikinakano <kabayaki3323@gmail.com>
2021-08-18 22:07:07 +0900

    chapter16

---
+++ JAVAunderstand/src/chapter16/Main16_6.java
@@ -0,0 +1,17 @@
+package chapter16;
+import java.util.*;
+
+public class Main16_6 {
+	public static void main(String[] args) {
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都", 255);
+		prefs.put("東京都", 1261);
+		prefs.put("熊本県", 181);
+		int tokyo = prefs.get("東京都");
+		System.out.println("東京都の人口は、" + tokyo);
+		prefs.remove("京都府");
+		prefs.put("熊本", 182);
+		int kumamoto = prefs.get("熊本県");
+		System.out.println("熊本県の人口は、" + kumamoto);
+	}
+}
